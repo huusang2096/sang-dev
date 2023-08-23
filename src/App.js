24f2fb7cbea99d6fc08ed292858b190ba2d1f74e
“Sang <hhsang@mmsofts.com>
2023-08-23 15:17:38 +0700

    change UI

--- src/App.js
+++ src/App.js
@@ -6,10 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <meta name="google-site-verification" content="2HjirpnFfoGHpZe6yLU9qGoKI-RnOqihKAbX31SXUEQ" />
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          Cười đi em!
-        </h1>
+        <h1>MY 1ST WEBSITE</h1>
+        <h2>Have a nice day!</h2>
       </header>
     </div>
   );
